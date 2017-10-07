dbcb6991b036db762d80e07009991d1eb403fc10
lin-meister <matthewcaus@gmail.com>
2017-10-07 08:49:24 -0700

    init commit

---
+++ wothappen/getdata.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, session, url_for, redirect, json
+
+import requests
+
+def getMessages(_roomId):
+  url = (
+    "https://api.ciscospark.com/v1/messages?roomId=%s" %_roomId+
+    "&max=1000"
+  )
+  headers = {
+    'Content-type': 'application/json; charset=utf-8',
+    'Authorization': 'Bearer NmZlNGIzNjktMTc3YS00MDM1LTk0NDAtNzQ0MzFkZWJmNzk0Zjc2M2E1YTItOWU1',
+  }
+  roomMessages = requests.get(url, headers=headers)
+  return roomMessages
+
+messages = getMessages("df53038c-1940-355e-aa24-e4bc8d67b64a")
+print(messages.text)
